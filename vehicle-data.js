e58665eec36a6da9fb3fe7bd660a2635c2040dce
christomuller1974-wq <christomuller1974@gmail.com>
2026-08-04 12:06:40 +0200

    Create vehicle-data.js
    Add shared vehicle make and model dataset

---
+++ vehicle-data.js
@@ -0,0 +1,171 @@
+// Shared CarScoutZA vehicle make-and-model dataset.
+// Used by vehicle upload, vehicle editing and vehicle search.
+
+window.CARSCOUT_VEHICLE_DATA = {
+  "Audi": [
+    "A1", "A3", "A4", "A5", "A6", "A7", "A8",
+    "Q2", "Q3", "Q5", "Q7", "Q8", "R8", "TT"
+  ],
+
+  "BAIC": [
+    "B40 Plus", "Beijing X55", "X25"
+  ],
+
+  "BMW": [
+    "1 Series", "2 Series", "3 Series", "4 Series",
+    "5 Series", "6 Series", "7 Series", "8 Series",
+    "X1", "X2", "X3", "X4", "X5", "X6", "X7",
+    "i3", "i4", "iX"
+  ],
+
+  "Chery": [
+    "Tiggo 4 Pro", "Tiggo 7 Pro", "Tiggo 8 Pro"
+  ],
+
+  "Citroën": [
+    "C3", "C3 Aircross", "C4", "C4 Cactus", "C5 Aircross"
+  ],
+
+  "Datsun": [
+    "GO", "GO+"
+  ],
+
+  "Fiat": [
+    "500", "500X", "Doblo", "Fullback", "Panda", "Tipo"
+  ],
+
+  "Ford": [
+    "EcoSport", "Everest", "Fiesta", "Figo", "Focus",
+    "Kuga", "Mustang", "Ranger", "Territory", "Tourneo"
+  ],
+
+  "GWM": [
+    "C20R", "H5", "P-Series", "Steed 5", "Steed 6", "Steed"
+  ],
+
+  "Haval": [
+    "H1", "H2", "H6", "H9", "Jolion"
+  ],
+
+  "Honda": [
+    "Amaze", "Ballade", "BR-V", "Brio", "Civic",
+    "CR-V", "Elevate", "Fit", "HR-V", "Jazz"
+  ],
+
+  "Hyundai": [
+    "Accent", "Atos", "Creta", "Elantra", "Exter",
+    "Grand Creta", "Grand i10", "H-1", "i20",
+    "i30", "Kona", "Santa Fe", "Staria", "Tucson", "Venue"
+  ],
+
+  "Isuzu": [
+    "D-Max", "KB", "MU-X"
+  ],
+
+  "JAC": [
+    "T6", "T8", "T9", "X200"
+  ],
+
+  "Jaguar": [
+    "E-Pace", "F-Pace", "F-Type", "I-Pace", "XE", "XF", "XJ"
+  ],
+
+  "Jeep": [
+    "Cherokee", "Compass", "Gladiator", "Grand Cherokee",
+    "Renegade", "Wrangler"
+  ],
+
+  "Kia": [
+    "Carens", "Carnival", "Cerato", "K2700", "Pegas",
+    "Picanto", "Rio", "Seltos", "Sonet", "Sorento",
+    "Sportage"
+  ],
+
+  "Land Rover": [
+    "Defender", "Discovery", "Discovery Sport",
+    "Freelander", "Range Rover", "Range Rover Evoque",
+    "Range Rover Sport", "Range Rover Velar"
+  ],
+
+  "Lexus": [
+    "ES", "GS", "IS", "LC", "LS", "LX", "NX", "RX", "UX"
+  ],
+
+  "Mahindra": [
+    "Bolero", "KUV100", "Pik Up", "Scorpio",
+    "Scorpio-N", "Thar", "XUV300", "XUV500", "XUV700"
+  ],
+
+  "Mazda": [
+    "Mazda2", "Mazda3", "Mazda6", "BT-50",
+    "CX-3", "CX-30", "CX-5", "CX-60"
+  ],
+
+  "Mercedes-Benz": [
+    "A-Class", "B-Class", "C-Class", "CLA", "CLS",
+    "E-Class", "G-Class", "GLA", "GLB", "GLC",
+    "GLE", "GLS", "S-Class", "V-Class", "Vito"
+  ],
+
+  "MINI": [
+    "Clubman", "Countryman", "Cooper", "Hatch"
+  ],
+
+  "Mitsubishi": [
+    "ASX", "Eclipse Cross", "Outlander",
+    "Pajero", "Pajero Sport", "Triton", "Xpander"
+  ],
+
+  "Nissan": [
+    "Almera", "Juke", "Magnite", "Micra", "Navara",
+    "NP200", "NP300", "Patrol", "Qashqai", "X-Trail"
+  ],
+
+  "Opel": [
+    "Adam", "Astra", "Combo", "Corsa", "Crossland",
+    "Grandland", "Mokka", "Zafira"
+  ],
+
+  "Peugeot": [
+    "2008", "208", "3008", "308", "5008", "Landtrek", "Partner"
+  ],
+
+  "Porsche": [
+    "718", "911", "Cayenne", "Macan", "Panamera", "Taycan"
+  ],
+
+  "Renault": [
+    "Captur", "Clio", "Duster", "Kiger", "Koleos",
+    "Kwid", "Megane", "Sandero", "Triber"
+  ],
+
+  "Subaru": [
+    "BRZ", "Forester", "Impreza", "Legacy",
+    "Outback", "WRX", "XV"
+  ],
+
+  "Suzuki": [
+    "Baleno", "Brezza", "Celerio", "Ciaz", "Dzire",
+    "Ertiga", "Fronx", "Grand Vitara", "Ignis",
+    "Jimny", "S-Presso", "Swift", "Vitara", "XL6"
+  ],
+
+  "Toyota": [
+    "Agya", "Avanza", "Aygo", "C-HR", "Camry",
+    "Corolla", "Corolla Cross", "Corolla Quest",
+    "Etios", "Fortuner", "GR86", "Hiace", "Hilux",
+    "Land Cruiser", "Prado", "Quantum", "RAV4",
+    "Rumion", "Starlet", "Urban Cruiser", "Vitz", "Yaris"
+  ],
+
+  "Volkswagen": [
+    "Amarok", "Caddy", "Golf", "Jetta", "Kombi",
+    "Polo", "Polo Vivo", "T-Cross", "T-Roc",
+    "Taigo", "Tiguan", "Touareg", "Transporter", "Up"
+  ],
+
+  "Volvo": [
+    "C40", "S60", "S90", "V40", "V60",
+    "XC40", "XC60", "XC90"
+  ]
+};
